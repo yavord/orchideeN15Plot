ba0847dc3ce73c7711f942648707635381239521
yav-d <yavor.d0@gmail.com>
2021-08-11 10:23:52 +0300

    singleProcess final plot

--- singleProcess.r
+++ singleProcess.r
@@ -10,7 +10,7 @@ theme_set(theme_minimal())
 # path and filename
 ncpath <- "/home/yavor/Documents/mint/wd/ncdf/input/"
 ncname <- "anspin.nc"
-varname <- "varProcess.csv"
+varname <- "process.csv"
 ncfname <- paste(ncpath,ncname,sep = "")
 varfname <- paste(ncpath,varname,sep = "")
 
@@ -28,35 +28,45 @@ plot_rows <- function(data, var_row) {
   to_char <- sapply(var_row,as.character)
   
   pool_array <- ncvar_get(data,to_char[1])
-  pool15_array <- ncvar_get(data,to_char[2])
+  pool_15_array <- ncvar_get(data,to_char[2])
   flux_array <- ncvar_get(data,to_char[3])
   flux15_array <- ncvar_get(data,to_char[4])
+  pool2_array <- ncvar_get(data,to_char[5])
+  pool2_15_array <- ncvar_get(data,to_char[6])
+
+  pft <- 1
 
   plot_df <- data.frame(
-    pool_array[4,],
-    pool15_array[4,],
-    flux_array[4,],
-    flux15_array[4,]
+    pool_array[pft,],
+    pool_15_array[pft,],
+    flux_array[pft,],
+    flux15_array[pft,],
+    pool2_array[pft,],
+    pool2_15_array[pft,]
   )
 
   return(
     ggplot(plot_df, aes(x=1:365))+
+    # ggplot(plot_df, aes(x=1:12))+
       labs(y="gN")+
       scale_x_continuous(name = "Day", breaks = seq(0,365,50))+
+      # scale_x_continuous(name = "Month", breaks = seq(1,12,1))+
       scale_y_continuous(trans = 'log10')+
       geom_line(aes(y=plot_df[,1]), color = 'darkred')+
       geom_line(aes(y=plot_df[,2]), color = 'darkred', linetype='dashed')+
       geom_line(aes(y=plot_df[,3]), color = 'steelblue')+
-      geom_line(aes(y=plot_df[,4]), color = 'steelblue', linetype='dashed')
+      geom_line(aes(y=plot_df[,4]), color = 'steelblue', linetype='dashed')+
+      geom_line(aes(y=plot_df[,5]), color = 'orange')+
+      geom_line(aes(y=plot_df[,6]), color = 'orange', linetype='dashed')
   )
 }
 
 # plots <- lapply(var_names[1:4,], plot_row, data = ncin)
 
-test <- plot_rows(ncin, var_names[3,])
+f_plot <- plot_rows(ncin, var_names[2,])
 ggsave(
   filename = '1.png',
-  plot = test,
+  plot = f_plot,
   device = 'png',
   bg = 'white'
 )
